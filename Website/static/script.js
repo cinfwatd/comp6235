81f8b96631cfa7aa8fa2ffa41464f5d7d79de6f4
Cinfwat Dogak <cdogakz@yahoo.com>
2017-01-05 02:18:53 +0000

    add sample map

---
+++ Website/static/script.js
@@ -0,0 +1,33 @@
+/**
+ * Js.
+ */
+// var API_KEY = 'AIzaSyDAPV7m3xJuLkXdEFXP1UAGV2-AcY9QJjU';
+var map;
+function initMap() {
+    console.log('yessss')
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: new google.maps.LatLng(2.8,-187.3),
+        mapTypeId: 'terrain'
+    });
+
+    // Create a <script> tag and set the USGS URL as the source.
+    var script = document.createElement('script');
+    // This example uses a local copy of the GeoJSON stored at
+    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+window.eqfeed_callback = function(results) {
+    for (var i = 0; i < results.features.length; i++) {
+        var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1],coords[0]);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
+    }
+};
