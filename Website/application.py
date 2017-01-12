2f399a0faa63cce038c605b76b96e8739039435d
Kurran <kurran.bharaj@gmail.com>
2017-01-12 19:30:32 +0000

    business getters

--- Website/application.py
+++ Website/application.py
@@ -1,5 +1,34 @@
+from pymongo import MongoClient
 from flask import Flask, session, redirect, url_for, escape, request, render_template, abort
 app = Flask(__name__)
+
+client = MongoClient()
+db = client.yelp
+#print(db.collection_names())
+
+
+def get_latitude(business_id):
+    for i in db.businesses.find({'business_id': business_id}, {"latitude": 1}):
+        return i['latitude']
+
+def get_longitude(business_id):
+    for i in db.businesses.find({'business_id': business_id}, {"longitude": 1}):
+        return i['longitude']
+
+def get_review_count(business_id):
+    for i in db.businesses.find({'business_id': business_id}, {"review_count": 1}):
+        return i['review_count']
+
+def get_star_rating(business_id):
+    for i in db.businesses.find({'business_id': business_id}, {"stars": 1}):
+        return i['stars']
+
+
+print(get_latitude('m5hMJ7SPIK7are8SykvlvA'))
+
+
+
+
 
 
 @app.route('/')
